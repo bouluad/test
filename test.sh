85d4eb8f08bba73a4ccc44fbec5e334a5322244e
bouluad <bouluad@users.noreply.github.com>
2024-05-10 18:00:44 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
         VERSION=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
         echo "::set-output name=version::$VERSION"
+
+        VERSION=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
+        if [ -z "$VERSION" ]; then
+          VERSION="1.0.0"
+        fi
+        echo "::set-output name=version::$VERSION"
