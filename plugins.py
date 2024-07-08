da3032e473088059aaaa226862d05ae56f6c55e7
bouluad <bouluad@users.noreply.github.com>
2024-07-08 18:09:07 +0200

    Update plugins.py

--- plugins.py
+++ plugins.py
@@ -1,13 +1,27 @@
 import subprocess
 import json
+import re
 
-# Jenkins credentials
-jenkins_url = "http://your-jenkins-server/pluginManager/api/json?depth=1"
+# Jenkins credentials and URL
+jenkins_url = "http://your-jenkins-server"
 username = "your-username"
 api_token = "your-api-token"
 
+# Function to create a valid filename from a URL
+def create_filename_from_url(url):
+    # Remove the protocol (http or https) and any non-alphanumeric characters
+    sanitized_url = re.sub(r'http[s]?://', '', url)
+    sanitized_url = re.sub(r'\W+', '_', sanitized_url)
+    return f"{sanitized_url}_plugins.json"
+
+# Create the complete Jenkins API URL
+api_url = f"{jenkins_url}/pluginManager/api/json?depth=1"
+
+# Create a filename based on the Jenkins server URL
+json_filename = create_filename_from_url(jenkins_url)
+
 # Curl command to get the list of installed plugins
-curl_command = f'curl -u {username}:{api_token} "{jenkins_url}"'
+curl_command = f'curl -u {username}:{api_token} "{api_url}"'
 
 try:
     result = subprocess.run(curl_command, shell=True, capture_output=True, text=True)
@@ -22,9 +36,9 @@ try:
         plugin_versions = {plugin['shortName']: plugin['version'] for plugin in plugins}
         
         # Save the dictionary to a JSON file
-        with open('plugins.json', 'w') as json_file:
+        with open(json_filename, 'w') as json_file:
             json.dump(plugin_versions, json_file, indent=4)
         
-        print("Plugin information has been saved to plugins.json")
+        print(f"Plugin information has been saved to {json_filename}")
 except Exception as e:
     print(f"Exception occurred: {str(e)}")
